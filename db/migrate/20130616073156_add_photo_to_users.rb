5f6aa03589069aeac1deef09ca94edce9b1fc6bb
Moonlight <moonlight612311521@gmail.com>
2013-06-19 20:12:54 +0800

    user photo update

--- db/migrate/20130616073156_add_photo_to_users.rb
+++ db/migrate/20130616073156_add_photo_to_users.rb
@@ -1,5 +1,5 @@
 class AddPhotoToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :photo, :string, :default => 0
+    add_column :users, :photo, :string, :default => "0"
   end
 end
