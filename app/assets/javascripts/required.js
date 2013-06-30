08e522f1423978514e09fa7e6ebb45b6ef6ef754
Song Yangyu <flyfy1@gmail.com>
2013-06-30 16:00:40 +0800

    Remote TurboLinks, Add Collapsible Sidebar
    1. Remote TurboLink to makesure JS file is loaded everypage
    2. Added Collapsible sidebar, changed the sidebar to be static (originally fixed) to support the collapsible sidebar.

--- app/assets/javascripts/required.js
+++ app/assets/javascripts/required.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.datepicker
-//= require turbolinks
 //= require jquery-fileupload/basic
 //= require jquery.ui.tooltip
 //= require bootstrap
