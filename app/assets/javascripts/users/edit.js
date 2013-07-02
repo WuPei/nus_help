18a940cb68a92ce196833cfb42c86c8e81421a38
Song Yangyu <flyfy1@gmail.com>
2013-07-03 00:35:17 +0800

    Cropping Done, with preview
    After the cropping, the cropped image would be saved to user table's
    :photo_small
    
    The css definition, #preview, in application.css.sass, needs to be
    updated because the current work-around, by setting the max-width to be
    a very large number, is not clean.
    
    The correct implementation should remove the max-width limit of img tag,
    which is previously set by bootstrap.

--- app/assets/javascripts/users/edit.js
+++ app/assets/javascripts/users/edit.js
@@ -13,19 +13,36 @@ $(function(){
       rW = nW / sW, // Width Ratio
       rH = nH / sH; // Height Ratio
   var prev = $('#preview');
-  prev.attr("width",sW);
-  prev.attr("height",sH);
 
   function update_crop(coords){
-    $("#crop_x").val(coords.x * rW);
-    $("#crop_y").val(coords.y * rH);
-    $("#crop_w").val(coords.w * rW);
-    $("#crop_h").val(coords.h * rH);
-
+    var aH = coords.h * rH;
+    var aW = coords.w * rW;
+    var aX = coords.x * rW;
+    var aY = coords.y * rH;
+    $("#crop_x").val(aX);
+    $("#crop_y").val(aY);
+    $("#crop_w").val(aW);
+    $("#crop_h").val(aH);
+    
+    // coords.h is the # of pixels on the screen
+    // (aH = coords.h * rH) is the actual # of pixels
+    // This is the pic to be shown in the preview part
+    // Height of preview part is 180px, needs to show that total # of px
+    //    Thus: 180 / aH = pH / nH,  where iH is the height of image in preview
+    //    (idea: (show pixel / actual pixel) is constant)
+    // Thus pH = 180 / aH * nH = (180 / aH) * nH, where (180 / aH) begin the ratio
+    var rpH = 180 / aH,  rpW = 180 / aW,
+        pH = rpH * nH,   pW = rpW * nW, pX = rpW * aX, pY = rpH * aY;
+    var ppW = Math.round(pW);
+    console.log("pH = " + pH + ", ppW = " + ppW);
     prev.css({
-      marginLeft: '-' + Math.round(coords.x) + 'px',
-      marginTop: '-' + Math.round(coords.y) + 'px'
+      width:  ppW + "px",
+      height: Math.round(pH) + "px",
+      marginLeft: "-" + Math.round(pX) + 'px',
+      marginTop: '-' + Math.round(pY) + 'px'
     });
+    console.log(prev.css("width"));
+    console.log(prev.css("height"));
   }
 
   $img.Jcrop({
@@ -34,6 +51,5 @@ $(function(){
     aspectRatio: 1,
     minSize: [180/rW,180/rH]
   });
-
 });
 
