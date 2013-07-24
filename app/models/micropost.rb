8e7d535bf2dfe807119f3b3bf9ff13669b40588f
Song Yangyu <flyfy1@gmail.com>
2013-07-25 00:06:02 +0800

    Bug fix for unable to display micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -15,7 +15,7 @@ class Micropost < ActiveRecord::Base
   accepts_nested_attributes_for :comments, allow_destroy: true
 
   default_scope -> { order('created_at DESC') }
-  validates :title, presence: true, length: {maximum: 25}
+  validates :title, presence: true, length: {maximum: 100}
   validates :user_id, presence: true
   #validates_associated :nus_module
   #validates_date :deadline
