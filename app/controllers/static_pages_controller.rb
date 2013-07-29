5c512ccec49bb764b1f0d6e451b4af998188c67b
wupei <henry.wupei@gmail.com>
2013-07-29 16:23:39 +0800

    If people's feed are less than 5, then all the posts in his websites will be shown on his help feeds

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,12 @@ class StaticPagesController < ApplicationController
       feed_user_items = current_user.feed
       feed_mod_items = current_user.mod_feed
       feed_items_join = (feed_user_items + feed_mod_items).uniq
-      @feed_items = feed_items_join.paginate(page: params[:page],:per_page=>15)
+      #show the new comers more microposts 
+      if feed_items_join.count <= 5
+        @feed_items = Micropost.all.paginate(page:params[:page], :per_page=>15)
+      else
+        @feed_items = feed_items_join.paginate(page: params[:page],:per_page=>15)
+      end 
       @microposts = Micropost.all.sort{|u,v| v.comments.count <=> u.comments.count}.first 10
 
     end
